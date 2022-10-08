cce3dc91dd701cd2f82242a345b57e7a9288f499
podvalniypiero <podvalniypiero@mail.ru>
2022-10-08 17:49:48 +0300

    Animation
    It was a little bit of practice in animation with the help of GSAP library

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from(".nav",{y:-20,duration:3,ease:"bounce"})
+gsap.from(".link",{opacity:0,duration:4,stagger:1,delay:4})
+gsap.from(".we",{y:30,opacity:0, ease:"circ", duration:3,delay:10})
+gsap.from(".click",{y:30,opacity:0,delay:13})
